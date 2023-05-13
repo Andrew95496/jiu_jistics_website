bc767d9a41995828289fa7dcd37c6c5a89a0d78d
Andrew Leacock <andrewleacock96@gmail.com>
2023-05-13 19:32:19 -0400

    replace lorem text (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,14 @@ export default function App() {
     <div>
       <div className='h1-wrapper'>
         <h1 className='h1-header headers'>Jiu Jistics</h1>
-        <p className='app-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos aliquid magni  
-          veniam quibusdam quod quidem placeat error tempora nostrum molestias, velit nihil, quia fugit neque
-          mollitia quam veritatis libero! Velit iste ex at quod aliquam accusantium nihil fugiat numquam, 
-          optio est minima ullam atque eius consectetur delectus incidunt autem, recusandae perspiciatis 
-          deleniti, quasi saepe aspernatur ab omnis. Ratione fuga vero, repellendus reiciendis dolores ipsa
-          necessitatibus veritatis assumenda voluptate voluptas itaque! Delectus debitis velit natus incidunt, 
-          hic id! Enim quas suscipit porro minima. Reprehenderit atque labore unde quia necessitatibus dolores
-          quidem debitis esse consequatur, quis pariatur tempora eligendi delectus rerum earum.</p>
+        <p className='app-text'>Jiu Jistics is an advanced analytics website for jiu jitsu. 
+        Our Mission is to provide users with the most in-depth analysis for all of the major 
+        competitors in all of the major competions. We see our site as being used to advanced
+        the sport in ways never imagined. users would be able to use jiu jistics to buy the 
+        best instructionals, see how is really best a what part of the game, or just for their
+        own peronal research. </p>
       </div>
-      <div className='our_mission-wrapper'>
+      {/* <div className='our_mission-wrapper'>
         <h2 className='h2-headers headers'>Our Mission</h2>
         <p className='app-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus quaerat vel maxime hic repellat
           soluta sit voluptates odit praesentium labore recusandae sed dicta facilis itaque corrupti cupiditate,
@@ -23,7 +21,7 @@ export default function App() {
           voluptate, ullam ducimus sit voluptas molestias maxime consequatur voluptatibus magnam magni velit
           officiis quidem! Maxime, laboriosam dolorum modi nemo eaque quo accusamus dolor debitis nisi repellendus
           asperiores error nihil fuga natus reiciendis? Error, dolorum.</p>
-      </div>
+      </div> */}
         
     </div>
   )
--- src/components/Athletes.tsx
+++ src/components/Athletes.tsx
@@ -7,14 +7,7 @@ export default function Athletes() {
     <div>
       <div>
         <h1 className='h1-header headers'>Athletes</h1>
-        <p className='app-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos aliquid magni
-          veniam quibusdam quod quidem placeat error tempora nostrum molestias, velit nihil, quia fugit neque
-          mollitia quam veritatis libero! Velit iste ex at quod aliquam accusantium nihil fugiat numquam,
-          optio est minima ullam atque eius consectetur delectus incidunt autem, recusandae perspiciatis
-          deleniti, quasi saepe aspernatur ab omnis. Ratione fuga vero, repellendus reiciendis dolores ipsa
-          necessitatibus veritatis assumenda voluptate voluptas itaque! Delectus debitis velit natus incidunt,
-          hic id! Enim quas suscipit porro minima. Reprehenderit atque labore unde quia necessitatibus dolores
-          quidem debitis esse consequatur, quis pariatur tempora eligendi delectus rerum earum.</p>
+        <p className='app-text'>COMING SOON</p>
       </div>
       
     </div>
